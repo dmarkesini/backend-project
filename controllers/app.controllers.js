c81b5ebb1d7cf5c98d8661af7214ef2ce3e34070
dmarkesini <markesinh@gmail.com>
2022-08-01 16:07:41 +0100

    removed console log from controller

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -3,6 +3,5 @@ const { selectTopics } = require("../models/app.models");
 exports.getTopics = (req, res) => {
   selectTopics().then((topics) => {
     res.status(200).send(topics);
-    console.log(topics, "in the controller");
   });
 };
